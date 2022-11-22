26bd41662e7aec9cb39375cb28b6f6cdedd83612
Nicholas Pfaff <contact@nicholaspfaff.ch>
2022-11-22 17:02:47 -0500

    Add basic folder structure

---
+++ scripts/sim2sim.py
@@ -0,0 +1,11 @@
+"""sim2sim entrypoint"""
+
+#!/bin/python3
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
---
+++ setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+setup(
+    name="sim2sim",
+    version="1.0.0",
+    packages=["sim2sim"],
+    install_requires=[
+        "numpy",
+        "matplotlib",
+        "pre-commit",
+        "py",
+        "pytest",
+        "pytest-parallel",
+        "trimesh",
+    ],
+)
