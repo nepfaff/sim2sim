22c9c9f21670f4fb9b41c48f972f93a33f2d407e
Nicholas Pfaff <contact@nicholaspfaff.ch>
2022-11-23 13:06:50 -0500

    Add simple logging capabilities
    NOTE: This is temporary and will likely be changed

--- sim2sim/logging/dynamic_logger_base.py
+++ sim2sim/logging/dynamic_logger_base.py
@@ -1,13 +1,31 @@
 from abc import ABC, abstractmethod
+from typing import Union
+import os
+
+from pydrake.all import MultibodyPlant
 
 
 class DynamicLoggerBase(ABC):
-    """TODO"""
+    """Dynamics logger base class."""
 
     def __init__(self, logging_frequency_hz: float, logging_path: str):
-        """TODO"""
+        """
+        :param logging_frequency_hz: The frequency at which we want to log at.
+        :param logging_path: The path to the directory that we want to write the log files to.
+        """
         self._logging_frequency_hz = logging_frequency_hz
         self._logging_path = logging_path
+
+        self._outer_plant: Union[MultibodyPlant, None] = None
+        self._inner_plant: Union[MultibodyPlant, None] = None
+
+        if not os.path.exists(logging_path):
+            os.mkdir(logging_path)
+
+    def add_plants(self, outer_plant: MultibodyPlant, inner_plant: MultibodyPlant) -> None:
+        """Add finalized plants."""
+        self._outer_plant = outer_plant
+        self._inner_plant = inner_plant
 
     @abstractmethod
     def log(self, **kwargs) -> None:
