6958c5d38a9d91704d5f262b472c2113021efaf2
Nicholas Pfaff <contact@nicholaspfaff.ch>
2022-11-24 14:40:07 -0500

    Document ImageGeneratorBase

--- sim2sim/images/image_generator_base.py
+++ sim2sim/images/image_generator_base.py
@@ -6,20 +6,26 @@ from pydrake.all import MultibodyPlant
 
 
 class ImageGeneratorBase(ABC):
+    """The image generator responsible for placing cameras and taking images."""
+
     def __init__(self, plant: MultibodyPlant):
         """
-
         :param plant: Unfinalized plant of the simulation environment.
         """
         self._plant = plant
 
     @abstractmethod
-    def generate_images(self) -> Tuple[np.ndarray, np.ndarray, np.ndarray, Union[np.ndarray, None]]:
+    def generate_images(
+        self,
+    ) -> Tuple[np.ndarray, np.ndarray, np.ndarray, Union[np.ndarray, None], Union[np.ndarray, None]]:
         """
+        Generate images using existing or new cameras.
 
-        :return: A tuple of (images, intrinsics, extrinsics, depth):
-            -
-            -
-            -
-            -
+        :return: A tuple of (images, intrinsics, extrinsics, depths, masks):
+            - images: The images.
+            - intrinsics: The intrinsic matrices associated with the images.
+            - extrinsics: The extrinsic matrices associated with the images.
+            - depths: The depth images associated with the images.
+            - masks: The segmentation masks associated with the images. Each image may have multiple associated masks.
         """
+        raise NotImplementedError
