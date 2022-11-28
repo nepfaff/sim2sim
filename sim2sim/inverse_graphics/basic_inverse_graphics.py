6f3283fed3f58110cd803824940b09df1c6436a0
teowzh <firezen123@hotmail.com>
2022-11-28 10:21:00 -0500

    created methods to apply mask

--- sim2sim/inverse_graphics/basic_inverse_graphics.py
+++ sim2sim/inverse_graphics/basic_inverse_graphics.py
@@ -17,11 +17,48 @@ class BasicInverseGraph(InverseGraphicsBase):
         self.extrinsics = extrinsics
         self.depth = depth
 
+    def checkMinMax(self, arr):
+        if np.max(arr) > 0:
+            newArr = np.where(arr > 0, -1, arr)
+        else:
+            return arr
+        return newArr
+
+    def applyMask(self, mask):
+        newMask = self.checkMinMax(mask)
+        maskImg = self.images.copy()
+        maskImg[:, :] = self.images.img[:, :] * newMask[:, :]
+        return maskImg
+
     def convertImage2RGBD(self):
         """TODO"""
         return o3d.geometry.RGBDImage.create_from_color_and_depth(self.images, self.depth)
 
     def convertImage2PC(self):
+        """TODO"""
+        # pcd = o3d.geometry.PointCloud()
+        # pcd.points = o3d.utility.Vector3dVector(self._images)
+        # self.pointClouds = pcd.points
+
+        if self.depth is None:
+            pcd = o3d.geometry.PointCloud.create_from_rgbd_image(
+                image=self.convertImage2RGBD(),
+                intrinsic=o3d.camera.PinholeCameraIntrinsic(intrinsic_matrix=self.intrinsics),
+                extrinsic=self.extrinsics,
+            )
+        else:
+            pcd = o3d.geometry.PointCloud.create_from_depth_image(
+                image=self.depth,
+                intrinsic=o3d.camera.PinholeCameraIntrinsic(intrinsic_matrix=self.intrinsics),
+                extrinsic=self.extrinsics,
+            )
+        pcd = pcd.transform(
+            [[1, 0, 0, 0], [0, -1, 0, 0], [0, 0, -1, 0], [0, 0, 0, 1]]
+        )  # Flip it, otherwise the pointcloud will be upside down
+
+        return pcd
+
+    def convertImage2PCWMask(self, mask):
         """TODO"""
         # pcd = o3d.geometry.PointCloud()
         # pcd.points = o3d.utility.Vector3dVector(self._images)
