a15b35708200eb170aa4ffd0a41368941624bf69
teowzh <firezen123@hotmail.com>
2022-12-10 13:49:31 -0500

    added parser

--- sim2sim/util/nerfStudioRun.py
+++ sim2sim/util/nerfStudioRun.py
@@ -2,14 +2,36 @@ import numpy as np
 import open3d as o3d
 import os, subprocess
 import json
+import argparse
 
 
 def preProcessData(type: str, input_path: str, output_path: str, xml_path: str):
-    if type is not "metashape":
+    if type != "metashape":
         os.system(f"ns-process-data {type} --data {input_path} --output-dir {output_path}")
     else:
         os.system(f"ns-process-data metashape --data {input_path} --xml {xml_path} --output-dir {output_path}")
 
 
+def runNerfStudio(algo_type: str, input_path: str):
+    os.system(f"ns-train {algo_type} --data {input_path}")
+
+
 if __name__ == "__main__":
-    main()
+
+    parser = argparse.ArgumentParser(
+        description="Run NerfStudio to generate meshes", formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    )
+    parser.add_argument("-p", "--preprocess", action="store_false", help="preprocess data to NeRF Studio format")
+    parser.add_argument("-t", "--type", help="data type for preprocess (video,images,polycam,insta360,record3d)")
+    parser.add_argument("-s", "--source", help="source folder path for preprocess")
+    parser.add_argument("-d", "--dest", help="destination folder path for preprocess")
+    parser.add_argument("-x", "--xml", help="xml file for metashape preprocessing")
+    parser.add_argument(
+        "-a",
+        "--algo",
+        help="algo for NeRF Generation (nerfacto,instant-ngp,mipnerf,semantic-nerfw,vanilla-nerf,tensorf,dnerf,phototourism)",
+    )
+    parser.add_argument("-i", "--input", help="input data Spath for NeRF Generation")
+    args = parser.parse_args()
+    config = vars(args)
+    print(config)
