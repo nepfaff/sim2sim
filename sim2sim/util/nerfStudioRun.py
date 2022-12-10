322b79447d916c232455540e9a1d3528c3b4c75c
teowzh <firezen123@hotmail.com>
2022-12-10 14:05:16 -0500

    created a simple run script for NeRF Studio

--- sim2sim/util/nerfStudioRun.py
+++ sim2sim/util/nerfStudioRun.py
@@ -21,17 +21,24 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         description="Run NerfStudio to generate meshes", formatter_class=argparse.ArgumentDefaultsHelpFormatter
     )
-    parser.add_argument("-p", "--preprocess", action="store_false", help="preprocess data to NeRF Studio format")
-    parser.add_argument("-t", "--type", help="data type for preprocess (video,images,polycam,insta360,record3d)")
-    parser.add_argument("-s", "--source", help="source folder path for preprocess")
-    parser.add_argument("-d", "--dest", help="destination folder path for preprocess")
-    parser.add_argument("-x", "--xml", help="xml file for metashape preprocessing")
+    parser.add_argument("-p", "--preprocess", action="store_true", help="preprocess data to NeRF Studio format")
+    parser.add_argument(
+        "-t", "--type", type=str, help="data type for preprocess (video,images,polycam,insta360,record3d)"
+    )
+    parser.add_argument("-s", "--source", type=str, help="source folder path for preprocess")
+    parser.add_argument("-d", "--dest", type=str, help="destination folder path for preprocess")
+    parser.add_argument("-x", "--xml", type=str, help="xml file for metashape preprocessing")
     parser.add_argument(
         "-a",
         "--algo",
+        type=str,
         help="algo for NeRF Generation (nerfacto,instant-ngp,mipnerf,semantic-nerfw,vanilla-nerf,tensorf,dnerf,phototourism)",
     )
     parser.add_argument("-i", "--input", help="input data Spath for NeRF Generation")
     args = parser.parse_args()
     config = vars(args)
-    print(config)
+
+    if config["preprocess"]:
+        preProcessData(config["type"], config["source"], config["dest"], config["xml"])
+
+    runNerfStudio(config["algo"], config["input"])
