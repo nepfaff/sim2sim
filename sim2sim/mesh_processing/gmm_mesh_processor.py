f33b93746551eca6fd0d35f69d1025a556cb498d
Nicholas Pfaff <contact@nicholaspfaff.ch>
2023-08-17 16:02:30 +0100

    Extend GMMMeshProcessor to N manipulands

--- sim2sim/mesh_processing/gmm_mesh_processor.py
+++ sim2sim/mesh_processing/gmm_mesh_processor.py
@@ -18,7 +18,10 @@ DIV_EPSILON = 1e-9
 
 
 class GMMMeshProcessor(MeshProcessorBase):
-    """Replaces the mesh with spheres obtained from fitting GMMs using Expectation Maximization."""
+    """
+    Replaces the mesh with spheres obtained from fitting GMMs using Expectation
+    Maximization.
+    """
 
     def __init__(
         self,
@@ -30,9 +33,10 @@ class GMMMeshProcessor(MeshProcessorBase):
         """
         :param logger: The logger.
         :param visualize: Whether to visualize the fitted spheres.
-        :param gmm_em_params: Parameters for GMM EM fitting. All must be valid arguments for
-            sklearn.mixture.GaussianMixture.
-        :param threshold_std: The standard deviation to use as a threshold for converting a GMM into a mesh.
+        :param gmm_em_params: Parameters for GMM EM fitting. All must be valid arguments
+            for sklearn.mixture.GaussianMixture.
+        :param threshold_std: The standard deviation to use as a threshold for
+            converting a GMM into a mesh.
         """
         super().__init__(logger)
 
@@ -42,54 +46,65 @@ class GMMMeshProcessor(MeshProcessorBase):
 
         self._logger.log(meta_data={"mesh_processing_GMM_EM": gmm_em_params})
 
-    def process_meshes(self, mesh: o3d.geometry.TriangleMesh) -> MeshProcessorResult:
-        tmesh = open3d_to_trimesh(mesh)
-        pts = trimesh.sample.sample_surface_even(tmesh, 10000)[0]
-        gmm = sklearn.mixture.GaussianMixture(**self._gmm_em_params)
-        gmm.fit(pts)
-        mean = gmm.means_
-        prec = gmm.precisions_
-        rotation_matrix, radii_inv, _ = np.linalg.svd(prec)
-        radii = np.sqrt(1.0 / (radii_inv + DIV_EPSILON))
+    def process_meshes(
+        self, meshes: List[o3d.geometry.TriangleMesh]
+    ) -> List[MeshProcessorResult]:
+        mesh_processor_results = []
+        for mesh in meshes:
+            tmesh = open3d_to_trimesh(mesh)
+            pts = trimesh.sample.sample_surface_even(tmesh, 10000)[0]
+            gmm = sklearn.mixture.GaussianMixture(**self._gmm_em_params)
+            gmm.fit(pts)
+            mean = gmm.means_
+            prec = gmm.precisions_
+            rotation_matrix, radii_inv, _ = np.linalg.svd(prec)
+            radii = np.sqrt(1.0 / (radii_inv + DIV_EPSILON))
 
-        centers: List[np.ndarray] = []
-        radius: List[np.ndarray] = []
-        ellipsoids: List[Ellipsoid] = []
+            centers: List[np.ndarray] = []
+            radius: List[np.ndarray] = []
+            ellipsoids: List[Ellipsoid] = []
 
-        for idx in range(self._gmm_em_params["n_components"]):
-            c_i, r_i = mean[idx], radii[idx]
-            centers.append(c_i)
-            radius.append(r_i)
-            rot_mat = np.eye(4)
-            rot_mat[:3, :3] = rotation_matrix[idx]
-            ellipsoid = Ellipsoid(
-                center=c_i, radius=r_i * self._threshold_std, scale=1, transform=rot_mat
-            )
-            ellipsoids.append(ellipsoid)
+            for idx in range(self._gmm_em_params["n_components"]):
+                c_i, r_i = mean[idx], radii[idx]
+                centers.append(c_i)
+                radius.append(r_i)
+                rot_mat = np.eye(4)
+                rot_mat[:3, :3] = rotation_matrix[idx]
+                ellipsoid = Ellipsoid(
+                    center=c_i,
+                    radius=r_i * self._threshold_std,
+                    scale=1,
+                    transform=rot_mat,
+                )
+                ellipsoids.append(ellipsoid)
 
-        if self._visualize:
-            scene = trimesh.scene.scene.Scene()
+            if self._visualize:
+                scene = trimesh.scene.scene.Scene()
+                for ellipsoid in ellipsoids:
+                    scene.add_geometry(ellipsoid)
+                open3d_pcd = mesh.sample_points_uniformly(number_of_points=10000)
+                trimesh_pcd = trimesh.points.PointCloud(
+                    np.asarray(open3d_pcd.points),
+                    colors=np.repeat([[0, 0, 150]], len(open3d_pcd.points), axis=0),
+                )
+                scene.add_geometry(trimesh_pcd)
+                scene.show()
+
+            analytical_ellipsoids = []
             for ellipsoid in ellipsoids:
-                scene.add_geometry(ellipsoid)
-            open3d_pcd = mesh.sample_points_uniformly(number_of_points=10000)
-            trimesh_pcd = trimesh.points.PointCloud(
-                np.asarray(open3d_pcd.points),
-                colors=np.repeat([[0, 0, 150]], len(open3d_pcd.points), axis=0),
-            )
-            scene.add_geometry(trimesh_pcd)
-            scene.show()
+                analytical_ellipsoids.append(
+                    {
+                        "name": "ellipsoid",
+                        "radii": np.asarray(ellipsoid.radius),
+                        "transform": np.asarray(ellipsoid.transform),
+                    }
+                )
 
-        analytical_ellipsoids = []
-        for ellipsoid in ellipsoids:
-            analytical_ellipsoids.append(
-                {
-                    "name": "ellipsoid",
-                    "radii": np.asarray(ellipsoid.radius),
-                    "transform": np.asarray(ellipsoid.transform),
-                }
+            mesh_processor_results.append(
+                MeshProcessorResult(
+                    result_type=MeshProcessorResult.ResultType.PRIMITIVE_INFO,
+                    primitive_info=analytical_ellipsoids,
+                )
             )
 
-        return MeshProcessorResult(
-            result_type=MeshProcessorResult.ResultType.PRIMITIVE_INFO,
-            primitive_info=analytical_ellipsoids,
-        )
+        return mesh_processor_results
