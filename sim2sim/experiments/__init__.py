3f2d8a9b539d898afaad2637622a4667e45142e5
Nicholas Pfaff <contact@nicholaspfaff.ch>
2023-08-18 09:01:29 +0100

    Replace interaction pushing experiment with planar pushing with two manipulands

--- sim2sim/experiments/__init__.py
+++ sim2sim/experiments/__init__.py
@@ -3,4 +3,3 @@ from .iiwa_manip import run_iiwa_manip
 from .floor_drop import run_floor_drop
 from .random_force import run_random_force
 from .planar_pushing import run_planar_pushing
-from .interaction_pushing import run_interaction_pushing
