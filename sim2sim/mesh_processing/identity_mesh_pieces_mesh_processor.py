e9935f894c8f0294037e2fc27c776778cd6614ba
Nicholas Pfaff <contact@nicholaspfaff.ch>
2023-08-28 18:10:54 -0400

    Add is_compliant option to IdentityMeshPiecesMeshProcessor for converting pirces from obj to vtk

--- sim2sim/mesh_processing/identity_mesh_pieces_mesh_processor.py
+++ sim2sim/mesh_processing/identity_mesh_pieces_mesh_processor.py
@@ -4,7 +4,7 @@ from typing import List
 import open3d as o3d
 
 from sim2sim.logging import DynamicLogger
-from sim2sim.util import MeshProcessorResult
+from sim2sim.util import MeshProcessorResult, convert_obj_to_vtk
 from .mesh_processor_base import MeshProcessorBase
 
 
@@ -14,19 +14,24 @@ class IdentityMeshPiecesMeshProcessor(MeshProcessorBase):
     returns them.
     """
 
-    def __init__(self, logger: DynamicLogger, mesh_pieces_paths: List[str]):
+    def __init__(
+        self, logger: DynamicLogger, mesh_pieces_paths: List[str], is_compliant: bool
+    ):
         """
         :param mesh_pieces_paths: The paths to the directories containing the mesh
             pieces to load.
+        :param is_compliant: Whether to convert the mesh pieces into VTK format to
+            simulate as compliant Hydroelastic objects.
         """
         super().__init__(logger)
         self._mesh_pieces_paths = mesh_pieces_paths
+        self._is_compliant = is_compliant
 
     def process_meshes(
         self, meshes: List[o3d.geometry.TriangleMesh]
     ) -> List[MeshProcessorResult]:
         mesh_processor_results = []
-        for mesh_pieces_path in self._mesh_pieces_paths:
+        for i, mesh_pieces_path in enumerate(self._mesh_pieces_paths):
             mesh_pieces = []
             with os.scandir(mesh_pieces_path) as paths:
                 for path in paths:
@@ -36,11 +41,34 @@ class IdentityMeshPiecesMeshProcessor(MeshProcessorBase):
                         )
                         mesh_pieces.append(mesh_piece)
 
-            mesh_processor_results.append(
-                MeshProcessorResult(
-                    result_type=MeshProcessorResult.ResultType.TRIANGLE_MESH,
-                    triangle_meshes=mesh_pieces,
+            if self._is_compliant:
+                vtk_pieces_path = os.path.join(
+                    self._logger.tmp_dir_path,
+                    f"vtk_mesh_pieces_{i}"
+                    if len(mesh_pieces) > 1
+                    else f"processed_mesh_{i}.vtk",
                 )
-            )
+                if not os.path.exists(vtk_pieces_path):
+                    os.mkdir(vtk_pieces_path)
+                vtk_paths = convert_obj_to_vtk(
+                    obj_meshes=mesh_pieces,
+                    output_path=vtk_pieces_path,
+                    tmp_folder_path=os.path.join(
+                        self._logger.tmp_dir_path, "convert_obj_to_vtk_tmp_folder"
+                    ),
+                )
+                mesh_processor_results.append(
+                    MeshProcessorResult(
+                        result_type=MeshProcessorResult.ResultType.VTK_PATHS,
+                        vtk_paths=vtk_paths,
+                    )
+                )
+            else:
+                mesh_processor_results.append(
+                    MeshProcessorResult(
+                        result_type=MeshProcessorResult.ResultType.TRIANGLE_MESH,
+                        triangle_meshes=mesh_pieces,
+                    )
+                )
 
         return mesh_processor_results
