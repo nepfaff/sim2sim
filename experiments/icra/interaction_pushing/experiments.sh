455aed4653d7ba0b8f649a3523b304b88424a485
Nicholas Pfaff <contact@nicholaspfaff.ch>
2023-09-08 20:48:24 -0400

    Fix supercloud script and update paths

--- experiments/icra/interaction_pushing/experiments.sh
+++ experiments/icra/interaction_pushing/experiments.sh
@@ -6,14 +6,14 @@ PREFIX="icra"
 # Anvil onto dumbell
 python scripts/evaluate_primitive_representation_collection.py \
 --paths '[
-    "/home/nep/robot_locomotion/DualSDF/datasets/representation_collection_dumbell_sept/",
-    "/home/nep/robot_locomotion/DualSDF/datasets/representation_collection_anvil_sept/"
+    "~/representation_collections/representation_collection_dumbell_sept/",
+    "~/representation_collections/representation_collection_anvil_sept/"
     ]' \
 --experiment_description experiments/icra/interaction_pushing/anvil_onto_dumbell.yaml \
 --eval_contact_model \
 --additional_collision_geometries_mesh_pieces_paths '[
-    "/home/nep/Desktop/coacd_vhacd_mesh_pieces/dumbell",
-    "/home/nep/Desktop/coacd_vhacd_mesh_pieces/anvil"
+    "~/coacd_vhacd_mesh_pieces/dumbell",
+    "~/coacd_vhacd_mesh_pieces/anvil"
     ]' \
 --num_trajectory_iou_samples 0 \
 --include_gt \
@@ -23,14 +23,14 @@ python scripts/evaluate_primitive_representation_collection.py \
 # Mustard onto tomato
 python scripts/evaluate_primitive_representation_collection.py \
 --paths '[
-    "/home/nep/robot_locomotion/DualSDF/datasets/representation_collection_mustard_ring3_nerfstudio_sept/",
-    "/home/nep/robot_locomotion/DualSDF/datasets/representation_collection_tomato_can_nerfstudio_sept/"
+    "~/representation_collections/representation_collection_mustard_ring3_nerfstudio_sept/",
+    "~/representation_collections/representation_collection_tomato_can_nerfstudio_sept/"
     ]' \
 --experiment_description experiments/icra/interaction_pushing/mustard_onto_tomato.yaml \
 --eval_contact_model \
 --additional_collision_geometries_mesh_pieces_paths '[
-    "/home/nep/Desktop/coacd_vhacd_mesh_pieces/mustard",
-    "/home/nep/Desktop/coacd_vhacd_mesh_pieces/tomato"
+    "~/coacd_vhacd_mesh_pieces/mustard",
+    "~/coacd_vhacd_mesh_pieces/tomato"
     ]' \
 --num_trajectory_iou_samples 0 \
 --include_gt \
@@ -40,14 +40,14 @@ python scripts/evaluate_primitive_representation_collection.py \
 # Spam onto anvil
 python scripts/evaluate_primitive_representation_collection.py \
 --paths '[
-    "/home/nep/robot_locomotion/DualSDF/datasets/representation_collection_spam_sept/",
-    "/home/nep/robot_locomotion/DualSDF/datasets/representation_collection_anvil_sept/"
+    "~/representation_collections/representation_collection_spam_sept/",
+    "~/representation_collections/representation_collection_anvil_sept/"
     ]' \
 --experiment_description experiments/icra/interaction_pushing/spam_onto_anvil.yaml \
 --eval_contact_model \
 --additional_collision_geometries_mesh_pieces_paths '[
-    "/home/nep/Desktop/coacd_vhacd_mesh_pieces/spam",
-    "/home/nep/Desktop/coacd_vhacd_mesh_pieces/anvil"
+    "~/coacd_vhacd_mesh_pieces/spam",
+    "~/coacd_vhacd_mesh_pieces/anvil"
     ]' \
 --num_trajectory_iou_samples 0 \
 --include_gt \
@@ -57,14 +57,14 @@ python scripts/evaluate_primitive_representation_collection.py \
 # Spam onto mustard
 python scripts/evaluate_primitive_representation_collection.py \
 --paths '[
-    "/home/nep/robot_locomotion/DualSDF/datasets/representation_collection_spam_sept/",
-    "/home/nep/robot_locomotion/DualSDF/datasets/representation_collection_mustard_ring3_nerfstudio_sept/"
+    "~/representation_collections/representation_collection_spam_sept/",
+    "~/representation_collections/representation_collection_mustard_ring3_nerfstudio_sept/"
     ]' \
 --experiment_description experiments/icra/interaction_pushing/spam_onto_mustard.yaml \
 --eval_contact_model \
 --additional_collision_geometries_mesh_pieces_paths '[
-    "/home/nep/Desktop/coacd_vhacd_mesh_pieces/spam",
-    "/home/nep/Desktop/coacd_vhacd_mesh_pieces/mustard"
+    "~/coacd_vhacd_mesh_pieces/spam",
+    "~/coacd_vhacd_mesh_pieces/mustard"
     ]' \
 --num_trajectory_iou_samples 0 \
 --include_gt \
@@ -74,14 +74,14 @@ python scripts/evaluate_primitive_representation_collection.py \
 # Tomato onto hammer
 python scripts/evaluate_primitive_representation_collection.py \
 --paths '[
-    "/home/nep/robot_locomotion/DualSDF/datasets/representation_collection_tomato_can_nerfstudio_sept/",
-    "/home/nep/robot_locomotion/DualSDF/datasets/representation_collection_hammer_sept/"
+    "~/representation_collections/representation_collection_tomato_can_nerfstudio_sept/",
+    "~/representation_collections/representation_collection_hammer_sept/"
     ]' \
 --experiment_description experiments/icra/interaction_pushing/tomato_onto_hammer.yaml \
 --eval_contact_model \
 --additional_collision_geometries_mesh_pieces_paths '[
-    "/home/nep/Desktop/coacd_vhacd_mesh_pieces/tomato",
-    "/home/nep/Desktop/coacd_vhacd_mesh_pieces/hammer"
+    "~/coacd_vhacd_mesh_pieces/tomato",
+    "~/coacd_vhacd_mesh_pieces/hammer"
     ]' \
 --num_trajectory_iou_samples 0 \
 --include_gt \
