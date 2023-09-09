b902dd69f86a0a4b6d59e92a7407fcba87bc4437
nepfaff <nep19@ic.ac.uk>
2023-09-08 21:48:48 -0400

    Improve logging paths

--- scripts/supercloud.sh
+++ scripts/supercloud.sh
@@ -10,12 +10,11 @@ source /etc/profile
 module load anaconda/2022b
 module load gurobi/gurobi-1000
 
-cd sim2sim
 . sim2sim_env/bin/activate
 
 # Dummy run to compile everything
-python scripts/run_experiment.py \
+python ./scripts/run_experiment.py \
 --experiment_description experiments/planar_pushing/box/mustard_raw_tsdf_vs_spheres_equation_error.yaml
 
 # Actual experiments
-bash scripts/experiments.sh 0
+bash ./scripts/experiments.sh 9
